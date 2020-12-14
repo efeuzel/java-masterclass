68d232cf1864867967cf6db0f0e2f06acc21150c
Efe UZEL <efeuzel@gmail.com>
2020-12-14 12:28:58 +0300

    adding CodingExercise3

---
+++ CodingChallenge3/src/BarkingDog.java
@@ -0,0 +1,10 @@
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if (barking && (hourOfDay >= 0) && (hourOfDay <= 23) && ((hourOfDay > 22) || (hourOfDay < 8))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
