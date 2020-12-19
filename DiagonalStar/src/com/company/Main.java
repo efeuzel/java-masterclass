dbae105e9ac4000523953430c485adb9b02f9ceb
Efe UZEL <efeuzel@gmail.com>
2020-12-19 18:57:16 +0300

    Adding DiagonalStar

---
+++ DiagonalStar/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printSquareStar(5);
+    }
+
+    public static void printSquareStar(int number) {
+
+        if (number >= 5) {
+            for (int row = 0; row < number; row++) {
+                for (int col = 0; col < number; col++) {
+                    if (row == 0 || row == (number - 1) ||
+                            col == 0 || col == (number - 1) ||
+                            row == col || col == (number - row-1)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+            }
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+}
