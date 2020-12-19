13624b67ad4d18399cef60a457cd99e6f5ee79c8
Efe UZEL <efeuzel@gmail.com>
2020-12-19 10:30:22 +0300

    Adding DigitSumChallenge

---
+++ DigitSumChallenge/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits(5));
+        System.out.println(sumDigits(51));
+        System.out.println(sumDigits(222));
+        System.out.println(sumDigits(1111));
+        System.out.println(sumDigits(99999));
+    }
+
+    public static int sumDigits(int number) {
+
+        if (number < 10) return -1;
+        int leastSignificantDigit;
+        int sumOfDigits = 0;
+        while(number > 0) {
+            leastSignificantDigit = number % 10;
+            sumOfDigits += leastSignificantDigit;
+            number = number / 10;
+        }
+        return sumOfDigits;
+    }
+}
