46e534168160a283cd9828d4a89dce83e5ef4d89
Efe UZEL <efeuzel@gmail.com>
2021-01-31 16:39:39 +0300

    Adding PackagesChallange

---
+++ PackagesChallange/src/com/uzel/test/Main.java
@@ -0,0 +1,13 @@
+package com.uzel.test;
+
+import com.uzel.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Series.nSum(5));
+        System.out.println(Series.factorial(5));
+        System.out.println(Series.fibonacci(5));
+    }
+
+}
---
+++ SeriesLibrary/src/com/uzel/Series.java
@@ -0,0 +1,29 @@
+package com.uzel;
+
+public class Series {
+    public static int nSum(int n) {
+        return n*(n+1)/2;
+    }
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for(int i = 2; i<= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static int fibonacci(int n) {
+        int pp = 0;
+        int p = 1;
+        int curr = 0;
+        for(int i = 2; i <= n; i++) {
+            curr = pp + p;
+            pp = p;
+            p = curr;
+        }
+        return curr;
+    }
+
+
+}
