bc1888bddd386dc24ffaabf88d7d2ed10bbad27d
Efe UZEL <efeuzel@gmail.com>
2021-02-10 15:22:49 +0300

    Adding multithreaded TCP server and client.

---
+++ EchoServer/src/com/uzel/Main.java
@@ -0,0 +1,24 @@
+package com.uzel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+
+            while(true) {
+                //Socket socket = serverSocket.accept(); //will block until client connects
+                new Echoer(serverSocket.accept()).start();
+
+            }
+        } catch (IOException e) {
+            System.out.println("Server exception " + e.getMessage());
+        }
+    }
+}
